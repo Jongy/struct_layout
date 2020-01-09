68839541d8142e38b5337b011ce92fc26dda9b7d
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2020-01-09 21:54:58 +0200

    Add more headers to include_all.c

--- linux/include_all.c
+++ linux/include_all.c
@@ -8,9 +8,15 @@
 #include <linux/skbuff.h>
 #include <linux/net.h>
 #include <net/sock.h>
+#include <net/protocol.h>
 
 #include <linux/mutex.h>
 #include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/mount.h>
 #include <linux/fs.h>
+
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+#include <linux/ftrace.h>
